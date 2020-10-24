f6ef66a01fb48bf6f80a91a10933362458ee6bdc
Chace Ritchie <chace98ritchie@gmail.com>
2020-10-24 19:47:39 -0400

    q4

--- jenkins/core/src/main/java/jenkins/model/IdStrategy.java
+++ jenkins/core/src/main/java/jenkins/model/IdStrategy.java
@@ -246,30 +246,20 @@ public abstract class IdStrategy extends AbstractDescribableImpl<IdStrategy> imp
                         }
                     } else if (c == '$') {
                         StringBuilder hex = new StringBuilder(4);
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
+                        int tempVar = 0;
+                        for(int j = 0; j < 4; j++) {
+	                        i++;
+	                        if (i < chars.length) {
+	                            hex.append(chars[i]);
+	                        } else {
+	                        	tempVar = 1;
+	                            break;
+	                        }
                         }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
+                        if(tempVar == 1) {
+                        	break;
                         }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
+                        
                         buf.append(Character.valueOf((char)Integer.parseInt(hex.toString(), 16)));
                     }
                 }
